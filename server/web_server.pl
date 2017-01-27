16c6efea3072b067b775a23d9363cdd42d01accd
cbasile <cbasile@zixcorp.com>
2017-01-27 11:16:27 -0600

    web_ui: Finished rough pass for the web_ui.

--- server/web_server.pl
+++ server/web_server.pl
@@ -12,6 +12,7 @@ server(Port) :-
 :- http_handler(/, welcome, [prefix]).
 :- http_handler('/memorize.html', begin_memorization, []).
 :- http_handler('/response.html', respond, []). 
+:- http_handler('/update.html', update,[]).
 
 %Set the title to 'Welcome' and call the predicate "welcome_page" with the request, the welcome page redirects to the memorization page.
 welcome(Request) :-
@@ -26,21 +27,41 @@ get_practice_set(KeySet) :-
 %Landing point for 'memorize.html'. Gets the current time, finds all entries that are due and prompts for them. Stores the result in 'entries2.pl' User logins: instead of 'entries2' 'entries_UID/USER_NAME/etc' 
 begin_memorization(_Request) :-
         get_practice_set(Z),	
-	create_ui(Z), store('entries2.pl').
+	create_ui(Z).
 
+%Landing point for 'update.html'. Updates the entry based on the users response. First pulls out the headers, then it converts the remaining practice back to a list of numbers (likewise the key), and calls the 'process' method.
+update(Request) :- 
+	http_parameters(Request, [key(Key,[]), rating(Rating,[]), practice_remaining(Practice,[])]),
+	atom_number(Key, H),
+	atom_number(Rating, Q),
+	get_list(Practice, RemainingList),
+	process(H,Q,RemainingList, NewPractice),
+       	store('entries2.pl'),
+	create_ui(NewPractice).	
+	
 %Landing point for 'response.html'
 respond(Request) :-
-	http_parameters(Request, [key(_Key,[]),answer(Answer,[]),practice_remaining(Practice,[])]), 
-	get_list(Practice, KeyList),
+	http_parameters(Request, [key(Key,[]),answer(Answer,[]),practice_remaining(Practice,[])]), 
 	reply_html_page(
 		title('Rate your response'),
-		paragraph(Practice)).
+		[\response_form(_Request, Key, Practice, Answer)]).
 
 %Prepare and prompt the form for the specified verses (this will be done several times)
+create_ui([-1]) :-
+	reply_html_page(
+		title('All done!'),
+		[\practice_end]).
 create_ui(Z) :-
 	reply_html_page(
 		title('Huzzah!'),
 		[\prompt_form(_Request, Z)]).
+
+practice_end -->
+	html_begin(h1),
+	['Well done!'],
+	html_end(h1),
+	html_begin(br),
+	['You have completed all outstanding practice for the day!'].
 
 welcome_page(_Request) -->
 		html_begin(p),
@@ -116,10 +137,22 @@ prompt_form(_Request, [H|T]) -->
 	},
 	html_begin(form(action('response.html'))),
 	['Hint: ',Hint, '<br/>','Your Answer: '],
-	html_begin(input(type(text), name(answer))),
+	html_begin(input(type(text), name(answer), autocomplete(off))),
 	html_begin(input(type(hidden), name(key), value(H))),
 	html_begin(input(type(hidden), name(practice_remaining), value(TailString))),
 	html_begin(input(type(submit), value('Submit'))).
+prompt_form(_Request, [H]) --> 
+	{
+		entry(H, Value, _N, _EF, _Date),
+		data(Value, Hint, _Answer)
+	},
+	html_begin(form(action('response.html'))),
+	['Hint: ', Hint, '<br/>', 'Your Answer: '],
+	html_begin(input(type(text), name(answer), autocomplete(off))),
+	html_begin(input(type(hidden), name(key), value(H))),
+	html_begin(input(type(hidden), name(practice_remaining), value("-1"))),
+	html_begin(input(type(submit), value('Submit'))).
+
 
 %This takes a list and returns it in a string
 get_string([H|T], TailString) :-
@@ -129,6 +162,7 @@ get_string([H|T], TailString) :-
 get_string([H], H).
 
 %Gets a number list from the string
+get_list("-1", []).
 get_list(String, List) :-
 	split_string(String, ",", "", AsList),
 	to_number_list(AsList, List).
@@ -142,6 +176,16 @@ to_number_list([H|T], NumberList) :-
 to_number_list([], []).
 
 %This response form provides the feed back (1-5 score of how easily it was remembered), and then the memorization algorithm will be called through once this is submitted. (Memorization: process(H,Q,T,NewT))
-response_form(_Request, [H|T], Answer) -->
-	html_begin(form(action())),
-	['Actual: ', H].
+response_form(_Request, Key, KeyList, Answer) -->
+	{
+		atom_number(Key, H),
+		entry(H, Value, _N, _EF, _Date),
+		data(Value, _Hint, Actual)
+	},
+	html_begin(form(action('update.html'))),
+		['Your response was: ', Answer, '<br>The actual answer was: ', Actual],
+	html_begin(input(type(text), name(rating), autocomplete=off)),
+	html_begin(input(type(hidden), name(key), value(Key))),
+	html_begin(input(type(hidden), name(practice_remaining), value(KeyList))),
+	html_begin(input(type(submit), value('Submit'))).
+	
